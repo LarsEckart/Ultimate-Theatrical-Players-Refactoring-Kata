b60bf01020b6ff0ecb4ef8fda6c7bf6dfca659d4
Lars Eckart <lars.eckart@proekspert.ee>
2020-03-02 12:02:37 +0200

    evil coder at work

--- src/main/java/xp/theatrical/exercise/StatementController.java
+++ src/main/java/xp/theatrical/exercise/StatementController.java
@@ -4,13 +4,13 @@ import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
@@ -31,44 +31,46 @@ class StatementController {
         var volumeCredits = 0;
 
         Invoice invoice = new Invoice(request.getServletPath().substring(11));
-        var result = String.format("Statement for %s\n", invoice.getCustomer());
+        var st = String.format("Statement for %s\n", invoice.getCustomer());
 
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
-        for (var perf : invoice.getPerformances()) {
-            var play = Maps.uniqueIndex(template.query("SELECT name, type FROM plays", (rs, rowNum) -> new Play(rs.getString("name"), rs.getString("type"))), this::getPlayStringFunction).get(perf.playID);
+        ArrayList<Performance> performances = invoice.getPerformances();
+        int i = 0;
+        while (i < performances.size()) {
+            Performance perf = performances.get(i);
+            var play = Maps.uniqueIndex(template.query("SELECT name, type FROM plays", (rs, rowNum) -> new Play(rs.getString("name"), rs.getString("type"))),
+                    this::getPlayStringFunction).get(perf.playID);
             var thisAmount = 0;
-
+            i++;
             switch (play.type) {
                 case "tragedy":
                     thisAmount = 40000;
-                    if (perf.audience > 30) thisAmount += 1000 * (perf.audience - 30);
+                    if (perf.audience > 30)
+                        thisAmount += 1000 * (perf.audience - 30);
                     break;
                 case "comedy":
                     thisAmount = 30000;
-                    if (perf.audience > 20) thisAmount += 10000 + 500 * (perf.audience - 20);
+                    if (perf.audience > 20)
+                        thisAmount += 10000 + 500 * (perf.audience - 20);
                     thisAmount += 300 * perf.audience;
                     break;
                 default:
                     throw new Error("unknown type: ${play.type}");
             }
-
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
             // add extra credit for every ten comedy attendees
             if ("comedy".equals(play.type))
                 volumeCredits += Math.floor(perf.audience / 5);
-
             // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+            st += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
             totalAmount += thisAmount;
         }
-
-        result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
-        result += String.format("You earned %s credits\n", volumeCredits);
-
+        st += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
+        st += String.format("You earned %s credits\n", volumeCredits);
         response.setCharacterEncoding("UTF-8");
-        return result;
+        return st;
     }
 
     private String getPlayStringFunction(Play p) {
